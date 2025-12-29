db9748d7628b20c3322a8726157fc80a1f1a64df
Andrii <kovratovskiy1999@gmail.com>
2025-12-29 18:13:18 -0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,9 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    
+
     const response = await fetch(`${BASE_URL}/api/events`);
     const { events } = await response.json();
-
     return (
         <section>
             <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
