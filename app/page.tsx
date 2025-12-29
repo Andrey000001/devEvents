4d09fc6d7cbab56d89357fca613b07c130e80bd5
Andrii <kovratovskiy1999@gmail.com>
2025-12-29 18:33:16 -0500

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Page = async () => {
     'use cache';
     cacheLife('hours')
 
-    const response = await fetch(`${BASE_URL}/api/events`);
+    const response = await fetch(`${BASE_URL}/api/events`,{next: {revalidate: 60}});
     const { events } = await response.json();
     return (
         <section>
