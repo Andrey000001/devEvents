041e36483edf82c98a307c0ec0173313d78c9b4e
Andrii <kovratovskiy1999@gmail.com>
2025-12-29 17:35:55 -0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ const Page = async () => {
     cacheLife('hours')
     // const response = await fetch(`${BASE_URL}/api/events`)
     // const {events} = await response.json()
-
   return (
     <section>
       <h1 className='text-center'>The Hub for Every Dev <br />Event You Can't Miss</h1>
